ede5098910ccc6a09e14dd8c5eab2f3d444ca71a
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2025-07-10 18:08:36 +0530

    first api is created

---
+++ app/lib/connectDb.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+}
+
+export async function connectDb(): Promise<typeof mongoose> {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log('MongoDB connected successfully!');
+    return mongoose;
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
+}
