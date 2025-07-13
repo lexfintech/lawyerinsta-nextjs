5065ce59683a36862f88737f281e8b82d7f76e18
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2025-07-13 15:48:42 +0530

    get-profiles api is created and lawyer schema is updated

--- app/models/lawyer.ts
+++ app/models/lawyer.ts
@@ -9,11 +9,7 @@ export interface ILawyer extends Document {
   mobile_Number: number;
   city: string[];
   WhatsApp_Number: number;
-  building_name: string;
-  landmark: string;
-  post_office: string;
-  country: string;
-  pincode: number;
+  address: string,
   intro_video_url: string;
   is_active: boolean;
   email_verified: boolean;
@@ -25,11 +21,16 @@ export interface ILawyer extends Document {
   door_no: string;
   profile_overview: string;
   profile_picture_url: string;
+  cover_picture_url: string;
+  education: string;
+  languages: string[];
+  bio: string;
   state_id: number;
   city_id: number;
   is_premium: boolean;
   premium_expires_at: Date;
-  area_of_expertise: String[];
+  area_of_expertise: string[];
+  cases_completed: number,
 }
 
 const LawyerSchema: Schema<ILawyer> = new mongoose.Schema(
@@ -43,11 +44,7 @@ const LawyerSchema: Schema<ILawyer> = new mongoose.Schema(
     city: { type: [String], required: true },
     area_of_expertise: { type: [String], default: [] },
     WhatsApp_Number: { type: Number, default: null },
-    building_name: { type: String, default: null },
-    landmark: { type: String, default: null },
-    post_office: { type: String, default: null },
-    country: { type: String, required: false },
-    pincode: { type: Number, default: null },
+    address: { type: String, default: null },
     intro_video_url: { type: String, default: null },
     is_active: { type: Boolean, default: false },
     email_verified: { type: Boolean, default: false },
@@ -58,11 +55,24 @@ const LawyerSchema: Schema<ILawyer> = new mongoose.Schema(
     practice_start_year: { type: Number, default: null },
     door_no: { type: String, default: null },
     profile_overview: { type: String, default: null },
-    profile_picture_url: { type: String, default: null },
+    profile_picture_url: {
+      type: String,
+      default:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fprofile_3135715&psig=AOvVaw20Qv8sJmFKALbu8dkSB9Ai&ust=1752485501328000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCf0LDDuY4DFQAAAAAdAAAAABAE',
+    },
+    cover_picture_url: {
+      type: String,
+      default:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Flawyer-banner&psig=AOvVaw1BuvjfB2bFOH_91-Cw1Ac0&ust=1752485551168000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIim4enDuY4DFQAAAAAdAAAAABAE',
+    },
+    education: { type: String, default: null },
+    languages: { type: [String], default: [] },
+    bio: { type: String, default: null },
     state_id: { type: Number, default: null },
     city_id: { type: Number, default: null },
     is_premium: { type: Boolean, default: false },
     premium_expires_at: { type: Date, default: null },
+    cases_completed: { type: Number, default: null },
   },
   { timestamps: true }, // adds createdAt and updatedAt automatically
 );
